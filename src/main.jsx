6a90a6b5dd6d179ebdefb60fd7c00db14a8fa247
Rakib Hossen <rakibhossen1660@gmail.com>
2023-05-17 22:32:22 +0600

    create nav items

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import router from './routes/Routes.jsx'
-import { RouterProvider } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import router from "./routes/Routes.jsx";
+import { RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div className="max-w-7xl mx-auto">
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </div>
+);
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,11 +1,19 @@
 import {
     createBrowserRouter,
   } from "react-router-dom";
+import Main from "../layout/Main";
+import Home from "../pages/Home/Home";
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        }
+      ]
     },
   ]);
 
