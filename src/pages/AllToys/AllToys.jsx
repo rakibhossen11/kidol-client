c57d9b4ed2e47b4bda155c73b36f35a373e5d1d2
Rakib Hossen <rakibhossen1660@gmail.com>
2023-05-18 10:29:17 +0600

    create all toys

--- src/pages/AllToys/AllToys.jsx
+++ src/pages/AllToys/AllToys.jsx
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import AllToysRow from "./AllToysRow";
 
 const AllToys = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [allToys, setAllToys] = useState([]);
+
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setAllToys(data));
+  }, []);
+
+  return (
+    <div>
+      <h2 className="text-5xl">Bookings: {allToys.length}</h2>
+      <div className="overflow-x-auto w-full">
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <th>Toy Image</th>
+            <th>Name</th>
+            <th>Seller Name</th>
+            <th>Seller Email</th>
+            <th>Category</th>
+            <th>Price</th>
+            <th>Rating</th>
+            <th>Available Quantity</th>
+            <th>Details</th> 
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {allToys.map((allToy) => (
+              <AllToysRow
+                key={allToy.id}
+                allToy={allToy}
+              ></AllToysRow>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default AllToys;
---
+++ src/pages/AllToys/AllToysRow.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const AllToysRow = ({allToy}) => {
+    const {id,name,seller,subCategory,price,quantity} = allToy;
+  return (
+    <tr>
+      <th>
+        <button className="btn btn-sm btn-circle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+      </th>
+      <td>
+          <div className="">
+          Toy Image
+          </div>
+      </td>
+            <th>{name}</th>
+            <th>{seller}</th>
+            <th>Seller Email</th>
+            <th>{subCategory}</th>
+            <th>{price}</th>
+            <th>Rating</th>
+            <th>{quantity}</th>
+            <th>Details</th> 
+    </tr>
+  );
+};
+
+export default AllToysRow;
