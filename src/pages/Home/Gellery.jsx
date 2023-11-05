5069f750713146b4341d3e18432f62cb9780e48f
Rakib Hossen <rakibhossen1660@gmail.com>
2023-11-05 22:12:06 +0600

    create hero section

--- src/pages/Home/Gellery.jsx
+++ src/pages/Home/Gellery.jsx
@@ -2,73 +2,16 @@ import React from "react";
 
 const Gellery = () => {
   return (
-    <div className="mt-10">
-      <h2 className="text-center font-display text-4xl">Our Gellary</h2>
-      <p className="text-center font-myFont">
+    <div className="mt-10 bg-Cyanblue">
+      <h2 className="text-center font-Ubuntuu text-4xl text-Blue">Our Gellary</h2>
+      <p className="text-center text-2xl font-Poppins">
         Welcome to our Sports Car Toy Gallery, where speed and style converge in
         miniature form! Get ready to rev up your imagination as we present a
         collection of sleek and stunning sports car toys that will captivate
         auto enthusiasts of all ages.
       </p>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-10" data-aos="fade-up"
-          data-aos-easing="ease-out-cubic"
-          data-aos-duration="2000">
-        <div
-          className="card card-side shadow-xl"
-        >
-          <figure>
-            <img
-              className="p-4"
-              src="https://image.made-in-china.com/201f0j00lsnVPhiWrMqH/Factory-Outlet-Fidget-Preschool-Educational-Plastic-Toys-Musical-Bus-Electric-Universal-Fun-Fruit-Car-Baby-Toys-Price-Baby-Kids-Children-Toys.webp"
-              alt=""
-            />
-          </figure>
-        </div>
-        <div className="card card-side shadow-xl">
-          <figure>
-            <img
-              className="p-4"
-              src="https://image.made-in-china.com/201f0j00HVEqmAflnhki/Licensed-12V-Child-Electric-Ride-on-Toy-Car-with-RC.webp"
-              alt=""
-            />
-          </figure>
-        </div>
-        <div className="card card-side shadow-xl">
-          <figure>
-            <img
-              className="p-4"
-              src="https://image.made-in-china.com/201f0j00cjkWCfvnlZbE/24V-2-Seater-Ride-on-Car-Kids-Electric-Toy-Car.webp"
-              alt=""
-            />
-          </figure>
-        </div>
-        <div className="card card-side shadow-xl">
-          <figure>
-            <img
-              className="p-4"
-              src="https://image.made-in-china.com/201f0j00GipcKPWJgabz/Licnesed-12V-Kids-Electric-Ride-on-Toy-Car-with-Remote-Control.webp"
-              alt=""
-            />
-          </figure>
-        </div>
-        <div className="card card-side shadow-xl">
-          <figure>
-            <img
-              className="p-4"
-              src="https://image.made-in-china.com/201f0j00kcQuwjEsbRrS/Kids-Model-Plastic-R-C-Toy-Car-1-26-Electric-Radio-Remote-Control-Toys-RC-Car.webp"
-              alt=""
-            />
-          </figure>
-        </div>
-        <div className="card  shadow-xl">
-          <figure>
-            <img
-              className="p-4"
-              src="https://image.made-in-china.com/201f0j00NtAUsDIhfezn/Battery-Operated-Toys-Child-Car-Kids-Electric-Car-Ride-on-Car.jpg"
-              alt=""
-            />
-          </figure>
-        </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-10">
+        
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,17 @@ export default {
   theme: {
     extend: {},
     fontFamily: {
-      display: ['Bebas Neue', 'sans-serif'],
-      myFont: ['Poppins', 'sans-serif'],
+      Bebas: ['Bebas Neue', 'sans-serif'],
+      Poppins: ['Poppins', 'sans-serif'],
       Ubuntuu: ['Ubuntu', 'sans-serif'],
     },
+    colors: {
+      Blue: '#003580',
+      Cyan: '#009fe3',
+      Yellow: '#feba02',
+      Zinc: '#666',
+      Cyanblue: '#f2f6fa',
+    }
   },
   plugins: [require("daisyui"),require('flowbite/plugin')],
 }
