8946d3fc8fc683726b93c2aca314d4bde4c21be7
Rakib Hossen <rakibhossen1660@gmail.com>
2023-11-06 08:41:00 +0600

    create card design

--- src/pages/Home/Gellery.jsx
+++ src/pages/Home/Gellery.jsx
@@ -3,16 +3,7 @@ import React from "react";
 const Gellery = () => {
   return (
     <div className="">
-      <h2 className="text-center bg-Yellow py-4 font-Ubuntuu text-4xl text-Blue">Our Gellary</h2>
-      <p className="text-center text-2xl font-Poppins">
-        Welcome to our Sports Car Toy Gallery, where speed and style converge in
-        miniature form! Get ready to rev up your imagination as we present a
-        collection of sleek and stunning sports car toys that will captivate
-        auto enthusiasts of all ages.
-      </p>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-10">
-        
-      </div>
+      <h2 className="text-center bg-Yellow py-4 font-Ubuntuu text-4xl text-Blue border-t-2 border-black">To be Happy!</h2>
     </div>
   );
 };
--- src/pages/Home/OurShop/OurShop.jsx
+++ src/pages/Home/OurShop/OurShop.jsx
@@ -10,9 +10,7 @@ const OurShop = () => {
       .then((data) => setProducts(data));
   }, []);
   return (
-    <div className="p-4">
-        <h2 className="text-center font-display text-4xl">Our Shop</h2>
-        <p className="text-center font-myFont mb-10">Explore our vast collection of toys, ranging from classic board games to cutting-edge robotics. Our friendly and knowledgeable staff is ready to assist you in finding the perfect toy for your child's age and interests. Enjoy a fun-filled shopping experience with regular promotions and a dedicated play area for kids.</p>
+    <div className="p-4 bg-Cyanblue border-t-2 border-Cyan">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         {products.map((product) => (
           <Product key={product._id} product={product}></Product>
